5eb152c7be3cf723dfe29d2e7154f3fe6f884e78
qamar345 <qamargill427@gmail.com>
2024-07-22 19:06:06 +0500

    develop admin for career

--- career_ezitech/src/App.jsx
+++ career_ezitech/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Register } from "./pages/Register";
-import { AdminDashboard } from "./admin/pages/AdminDashboard";
-import { NewApplicants } from "./admin/pages/NewApplicants";
-import { ProfileView } from "./admin/pages/ProfileView";
-import { AdminLogin } from "./admin/pages/AdminLogin";
+import { AdminDashboard } from "./pages/admin/pages/AdminDashboard";
+import { NewApplicants } from "./pages/admin/pages/NewApplicants";
+import { ProfileView } from "./pages/admin/pages/ProfileView";
+import { AdminLogin } from "./pages/admin/pages/AdminLogin";
 
 function App() {
   return (
--- career_ezitech/src/pages/admin/pages/AdminDashboard.jsx
+++ career_ezitech/src/pages/admin/pages/AdminDashboard.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { AdminSidebar } from "../components/AdminSidebar";
 import { AdminTopbar } from "../components/AdminTopbar";
 import { AdminFooter } from "../components/AdminFooter";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 
 export const AdminDashboard = () => {
   const [data, setData] = useState([]);
